920dfc7bf2335e962ee51507a246817be03e890e
miharyjoe <miharyjoel@gmail.com>
2025-01-22 20:23:39 +0300

    feat: add login form and shadcn components

---
+++ components/login-form.tsx
@@ -0,0 +1,44 @@
+import { cn } from "@/lib/utils";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import SignIn from "./sign-in";
+import { signIn } from "@/auth";
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader className="text-center">
+          <CardTitle className="text-xl">Welcome back</CardTitle>
+          <CardDescription>Login with your Spotify account</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form
+            action={async () => {
+              "use server";
+              await signIn("spotify");
+            }}
+          >
+            <div className="grid gap-6">
+              <div className="flex flex-col gap-4">
+                <SignIn />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+      <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary  ">
+        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
+        and <a href="#">Privacy Policy</a>.
+      </div>
+    </div>
+  );
+}
